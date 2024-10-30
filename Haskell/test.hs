1fe0efee8e5c4ae1fdb812a3a1a6e6c026a7fc65
ihpar <ismail.hakki.parlak@gmail.com>
2024-10-30 19:26:10 +0300

    deletes garbage files

--- Haskell/test.hs
+++
@@ -1,8 +0,0 @@
-main = do
-  putStr "Merhaba"
-  putStrLn " adın ne?"
-  ad <- getLine
-  putStrLn $ "Memnun oldum " ++ ad
-  putStrLn "Kaç yaşındasın?"
-  yas :: Int <- readLn
-  putStrLn ("Demek " ++ show (2024 - yas) ++ " doğumlusun.")
